108ec52d31a39c48ecab0cb962274c784a0272d5
MoetazJ <jaouedmoetaz@gmail.com>
2024-03-22 16:57:26 +0100

    V2

--- bdd/films.sql
+++ bdd/films.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 12 mars 2024 à 20:12
+-- Généré le : ven. 22 mars 2024 à 15:52
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.4.9
 
@@ -29,121 +29,53 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE IF NOT EXISTS `comments` (
-  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
-  `movie_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `corps` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`comment_id`),
-  KEY `movie_id` (`movie_id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+                                          `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+                                          `movie_id` int(11) DEFAULT NULL,
+                                          `user_id` int(11) DEFAULT NULL,
+                                          `corps` varchar(255) DEFAULT NULL,
+                                          PRIMARY KEY (`comment_id`),
+                                          KEY `movie_id` (`movie_id`),
+                                          KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `comments`
 --
 
 INSERT INTO `comments` (`comment_id`, `movie_id`, `user_id`, `corps`) VALUES
-(1, 1, 1, 'Amazing movie! Really makes you think.'),
-(2, 2, 1, 'A classic. Always worth a rewatch.');
+                                                                          (5, 5, 72, 'Wowwww'),
+                                                                          (7, 3, 72, 'Excellent film'),
+                                                                          (8, 3, 73, 'On comprends rien'),
+                                                                          (9, 2, 72, 'Bon film');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `details_paniers`
+-- Structure de la table `films`
 --
 
-DROP TABLE IF EXISTS `details_paniers`;
-CREATE TABLE IF NOT EXISTS `details_paniers` (
-  `details_id` int(11) NOT NULL AUTO_INCREMENT,
-  `panier_id` int(11) NOT NULL,
-  `movie_id` int(11) NOT NULL,
-  `quantity` int(11) DEFAULT '1',
-  PRIMARY KEY (`details_id`),
-  UNIQUE KEY `panier_movie` (`panier_id`,`movie_id`),
-  KEY `movie_id` (`movie_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `films`;
+CREATE TABLE IF NOT EXISTS `films` (
+                                       `movie_id` int(11) NOT NULL AUTO_INCREMENT,
+                                       `name` varchar(255) NOT NULL,
+                                       `release_date` date DEFAULT NULL,
+                                       `description` text,
+                                       `url` varchar(255) DEFAULT NULL,
+                                       PRIMARY KEY (`movie_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `details_paniers`
+-- Déchargement des données de la table `films`
 --
 
-INSERT INTO `details_paniers` (`details_id`, `panier_id`, `movie_id`, `quantity`) VALUES
-(1, 1, 1, 1),
-(2, 1, 2, 2);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `movies`
---
-
-DROP TABLE IF EXISTS `movies`;
-CREATE TABLE IF NOT EXISTS `movies` (
-  `movie_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `release_date` date DEFAULT NULL,
-  `description` text,
-  `thumbnail_url` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`movie_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `movies`
---
-
-INSERT INTO `movies` (`movie_id`, `name`, `release_date`, `description`, `thumbnail_url`) VALUES
-(1, 'Inception', '2010-07-16', 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.', 'C:\\Users\\jaoue\\Desktop\\S8\\Films\\front\\Front\\src\\assets\\inception.webp'),
-(2, 'The Matrix', '1999-03-31', 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.', NULL);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `paniers`
---
-
-DROP TABLE IF EXISTS `paniers`;
-CREATE TABLE IF NOT EXISTS `paniers` (
-  `panier_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`panier_id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `paniers`
---
-
-INSERT INTO `paniers` (`panier_id`, `user_id`) VALUES
-(1, 1),
-(2, 2);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `rentals`
---
-
-DROP TABLE IF EXISTS `rentals`;
-CREATE TABLE IF NOT EXISTS `rentals` (
-  `rental_id` int(11) NOT NULL AUTO_INCREMENT,
-  `movie_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `panier_id` int(11) DEFAULT NULL,
-  `rental_date` date NOT NULL,
-  `return_date` date DEFAULT NULL,
-  PRIMARY KEY (`rental_id`),
-  KEY `movie_id` (`movie_id`),
-  KEY `user_id` (`user_id`),
-  KEY `panier_id` (`panier_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `rentals`
---
-
-INSERT INTO `rentals` (`rental_id`, `movie_id`, `user_id`, `panier_id`, `rental_date`, `return_date`) VALUES
-(1, 1, 1, 1, '2024-03-15', '2024-03-22'),
-(2, 2, 1, 1, '2024-03-15', '2024-03-29');
+INSERT INTO `films` (`movie_id`, `name`, `release_date`, `description`, `url`) VALUES
+                                                                                   (2, 'The Matrix', '1999-03-31', 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.', 'Matrix.webp'),
+                                                                                   (3, 'Inception ', '2024-03-04', 'Meilleur film', 'inception.webp'),
+                                                                                   (5, 'Interstellar', '2014-11-07', 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity\'s survival.', 'interstellar.jpeg'),
+                                                                                   (7, 'Arrival', '2016-11-11', 'A linguist works with the military to communicate with alien lifeforms after twelve mysterious spacecraft appear around the world.', 'arrival.jpeg'),
+                                                                                   (10, 'Moon', '2009-07-17', 'Astronaut Sam Bell has a quintessentially personal encounter toward the end of his three-year stint on the Moon, where he, working alongside his computer, GERTY, sends back to Earth parcels of a resource that has helped diminish our planet\'s power problems.', 'moon.webp'),
+                                                                                   (15, 'test', NULL, 'test', 'test.jpg'),
+                                                                                   (16, 'creed', NULL, 'Boxe', 'boce.jpg');
 
 -- --------------------------------------------------------
 
@@ -153,23 +85,25 @@ INSERT INTO `rentals` (`rental_id`, `movie_id`, `user_id`, `panier_id`, `rental_
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password_hash` char(60) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `is_admin` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+                                       `user_id` int(11) NOT NULL AUTO_INCREMENT,
+                                       `username` varchar(70) NOT NULL,
+                                       `password` varchar(70) NOT NULL,
+                                       `is_admin` tinyint(1) NOT NULL DEFAULT '0',
+                                       `active` tinyint(1) NOT NULL DEFAULT '1',
+                                       PRIMARY KEY (`user_id`),
+                                       UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`user_id`, `username`, `password_hash`, `email`, `is_admin`) VALUES
-(1, 'john_doe', 'hashed_password_john', 'john.doe@example.com', 0),
-(2, 'admin_user', 'hashed_password_admin', 'admin@example.com', 1);
+INSERT INTO `users` (`user_id`, `username`, `password`, `is_admin`, `active`) VALUES
+                                                                                  (1, 'john_doe', 'hashed_password_john', 1, 0),
+                                                                                  (20, 'mo', 'mo', 1, 1),
+                                                                                  (72, 'user1', 'ok', 0, 1),
+                                                                                  (73, 'user2', 'ok', 1, 0),
+                                                                                  (74, 'eric', 'ok', 0, 1);
 
 --
 -- Contraintes pour les tables déchargées
@@ -179,29 +113,8 @@ INSERT INTO `users` (`user_id`, `username`, `password_hash`, `email`, `is_admin`
 -- Contraintes pour la table `comments`
 --
 ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`movie_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `details_paniers`
---
-ALTER TABLE `details_paniers`
-  ADD CONSTRAINT `details_paniers_ibfk_1` FOREIGN KEY (`panier_id`) REFERENCES `paniers` (`panier_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `details_paniers_ibfk_2` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`movie_id`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `paniers`
---
-ALTER TABLE `paniers`
-  ADD CONSTRAINT `paniers_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `rentals`
---
-ALTER TABLE `rentals`
-  ADD CONSTRAINT `rentals_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`movie_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `rentals_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `rentals_ibfk_3` FOREIGN KEY (`panier_id`) REFERENCES `paniers` (`panier_id`) ON DELETE CASCADE;
+    ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `films` (`movie_id`) ON DELETE CASCADE,
+    ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
